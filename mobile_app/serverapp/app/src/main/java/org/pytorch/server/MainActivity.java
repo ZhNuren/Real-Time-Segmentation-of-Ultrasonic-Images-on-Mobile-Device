8e4aa1325a0ecec748fcfd061f29780128193048
aidar <ai.myrzakhan@gmail.com>
2023-11-21 19:35:49 +0400

    include server files

---
+++ mobile_app/serverapp/app/src/main/java/org/pytorch/server/MainActivity.java
@@ -0,0 +1,119 @@
+package org.pytorch.server;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import org.pytorch.imagesegmentation.R;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ServerSocket serverSocket;
+
+    Thread serverThread = null;
+    private static final int SERVER_PORT = 8080;
+
+    private MediaProjectionManager projectionManager;
+    private MediaProjection mediaProjection;
+    private VirtualDisplay virtualDisplay;
+    private ImageReader imageReader;
+    private static final int REQUEST_CODE_CAPTURE_PERM = 1234;
+
+
+
+    private Socket clientSocket;
+    private volatile boolean isServerRunning = true; // Flag to control server thread
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        projectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        Button startServerButton = findViewById(R.id.startserver);
+        Button stopServerButton = findViewById(R.id.stopButton);
+        stopServerButton.setEnabled(false);
+
+        startServerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startServerButton.setEnabled(false);
+                stopServerButton.setEnabled(true);
+                Intent captureIntent = projectionManager.createScreenCaptureIntent();
+                startActivityForResult(captureIntent, REQUEST_CODE_CAPTURE_PERM);
+
+            }
+        });
+        stopServerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Stop the MediaProjectionService
+                startServerButton.setEnabled(true);
+                stopServerButton.setEnabled(false);
+                Intent serviceIntent = new Intent(MainActivity.this, MediaProjectionService.class);
+                stopService(serviceIntent);
+            }
+        });
+
+
+
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CAPTURE_PERM && resultCode == RESULT_OK) {
+            Intent serviceIntent = new Intent(this, MediaProjectionService.class);
+            serviceIntent.putExtra("resultCode", resultCode);
+            serviceIntent.putExtra("data", data);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceIntent);
+            } else {
+                startService(serviceIntent);
+            }
+
+        } else {
+            Button startServer = findViewById(R.id.startserver);
+            startServer.setEnabled(true);
+        }
+    }
+
+
+}
---
+++ mobile_app/serverapp/app/src/main/java/org/pytorch/server/MediaProjectionService.java
@@ -0,0 +1,281 @@
+package org.pytorch.server;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ServiceInfo;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.util.DisplayMetrics;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import org.pytorch.imagesegmentation.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class MediaProjectionService extends Service {
+
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "MediaProjectionChannel";
+
+    private MediaProjectionManager projectionManager;
+
+    private ServerSocket serverSocket;
+    private static final int SERVER_PORT = 8080;
+    private Socket clientSocket;
+    private HandlerThread handlerThread;
+    private Handler backgroundHandler;
+    private volatile boolean isServerRunning = true; // Flag to control server thread
+
+    private MediaProjection mediaProjection;
+    private final byte[] FRAME_DELIMITER = "FRAME_END".getBytes();
+
+    private VirtualDisplay virtualDisplay;
+    private ImageReader imageReader;
+    private int screenDensity, width, height;
+    private static final int desiredFrameRate = 5;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            Notification notification = buildNotification(); // Your method to build a notification
+            startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION);
+        } else {
+            startForeground(NOTIFICATION_ID, buildNotification());
+        }    }
+
+    private void startServer() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(SERVER_PORT);
+                    while (isServerRunning) {
+                        clientSocket = serverSocket.accept();
+                        // Client connected
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void sendData(byte[] data) {
+        try {
+            if(clientSocket != null && !clientSocket.isClosed()) {
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write(data);
+                outputStream.write(FRAME_DELIMITER);
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        int resultCode = intent.getIntExtra("resultCode", Activity.RESULT_CANCELED);
+        Intent projectionData = intent.getParcelableExtra("data");
+
+        projectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        mediaProjection = projectionManager.getMediaProjection(resultCode, projectionData);
+
+        setupScreenCapture(); // Set up screen capture parameters and start capturing
+        startScreenCapture(); // Start screen capture
+
+        startServer();
+        return START_STICKY;
+    }
+
+    private Bitmap centerCropToSquare(Bitmap bitmap) {
+        // Dimensions for the resulting square
+        int squareSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+
+        // Calculating the top left corner of the crop area
+        int x = 0; // No cropping from sides as the width is already 480
+        int y = (bitmap.getHeight() - squareSize) / 2; // Cropping from top and bottom
+
+        // Creating the cropped bitmap
+        Bitmap croppedBitmap = Bitmap.createBitmap(bitmap, x, y, squareSize, squareSize);
+
+
+        return croppedBitmap;
+    }
+
+    private void setupScreenCapture() {
+        // Initialize screen capture parameters like screenDensity, width, and height
+        // This can be done similar to how it's done in MainActivity
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        screenDensity = metrics.densityDpi;
+        width = metrics.widthPixels;
+        height = metrics.heightPixels;
+
+        // Setup ImageReader
+        imageReader = ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 2);
+        handlerThread = new HandlerThread("ScreenCaptureHandler");
+        handlerThread.start();
+        backgroundHandler = new Handler(handlerThread.getLooper());
+
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                while (isServerRunning) {
+                    try (Image img = reader.acquireLatestImage()) {
+                        if (img != null) {
+                            Image.Plane[] planes = img.getPlanes();
+                            if (planes[0].getBuffer() == null) {
+                                continue; // Skip if buffer is not available
+                            }
+
+                            ByteBuffer buffer = planes[0].getBuffer();
+                            int pixelStride = planes[0].getPixelStride();
+                            int rowStride = planes[0].getRowStride();
+                            int rowPadding = rowStride - pixelStride * width;
+
+                            Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+                            bitmap.copyPixelsFromBuffer(buffer);
+                            bitmap = centerCropToSquare(bitmap);
+                            bitmap = Bitmap.createScaledBitmap(bitmap, 256, 256, true);
+                            // Compress the Bitmap to JPEG
+                            try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                                byte[] data = stream.toByteArray();
+                                sendData(data); // Send data to the client
+                            }
+
+                            // Control the frame rate (optional)
+                            Thread.sleep(1000 / desiredFrameRate);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, backgroundHandler);
+    }
+
+    private void startScreenCapture() {
+        virtualDisplay = mediaProjection.createVirtualDisplay("ScreenCapture",
+                width, height, screenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                imageReader.getSurface(), null, null);
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Media Projection",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel.setDescription("Media Projection Service");
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(false);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    private Notification buildNotification() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+
+        PendingIntent pendingIntent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // For Android 12 (API level 31) and above, specify the PendingIntent mutability
+            pendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    notificationIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+        } else {
+            // For older versions, use the existing method
+            pendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    notificationIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+        }
+
+        return new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Media Projection Service")
+                .setContentText("Recording screen")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                .build();
+    }
+
+    private void stopServer() {
+        isServerRunning = false; // Signal the server thread to stop
+        try {
+            if (clientSocket != null && !clientSocket.isClosed()) {
+                clientSocket.close(); // Close client socket
+            }
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close(); // Close server socket
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopServer();
+        isServerRunning = false; // Signal the server thread to stop
+        if (virtualDisplay != null) {
+            virtualDisplay.release(); // Release the virtual display
+        }
+        if (mediaProjection != null) {
+            mediaProjection.stop(); // Stop the media projection
+        }
+        if (serverSocket != null && !serverSocket.isClosed()) {
+            try {
+                serverSocket.close(); // Close the server socket
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
